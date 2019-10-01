9578ad7e5a81352a4cf18df4e6ef6fcc87ec8c58
Francis Gichuhi <francisgichh@gmail.com>
2019-09-30 21:16:09 -0400

    Data types

---
+++ data-types.js
@@ -0,0 +1,23 @@
+//var myNum = 9
+
+
+//let myObj = {}
+
+var myArray = []
+
+//var today = new Date()
+
+//var xMas2018 = new Date('2018-12-25')
+
+var person = {
+    firstName: 'Franso',
+    lastName: 'Gichh',
+
+    toString: function() {
+    return this.firstName + ' -- ' + this.lastName
+    }
+    person.toString()
+}
+    
+}
+
