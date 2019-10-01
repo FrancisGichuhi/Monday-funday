0f7b3fb736cc5e73eba075b0bca5cd8d6592dcff
Francis Gichuhi <francisgichh@gmail.com>
2019-09-30 21:44:46 -0400

    Cars Exercise

---
+++ objects-in-array.js
@@ -0,0 +1,17 @@
+
+let cars = [{
+
+    make: 'chevy',
+    model: 'volt',
+    year: 2012
+},   {
+    make: 'tesla',
+    model: '3',
+    year: 2017
+},   {
+    make: 'nissan',
+    model: 'leaf',
+    year: 2013
+}]
+
+console.log(cars)
