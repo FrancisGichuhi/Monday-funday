fd3fcc2a3a90e763bb04d25f55c874ee72773f26
Francis Gichuhi <francisgichh@gmail.com>
2019-10-02 21:22:29 -0400

    car-mapping-reduce-filter

--- car-map-reduce-filter.js
+++ car-map-reduce-filter.js
@@ -26,4 +26,16 @@ let year = cars.filter( (car) => {
     return car.year === 2019
 })
 
-console.log( year )
+
+
+// return an object for each car that includes the existing data as well
+//as a single full string property named 'display'
+// for example '2012 ford fussion'
+
+let displayData = year.map((car) => {
+    let result = car
+    result.display = car.year + ' ' + car.make + ' ' + car.model
+    return result
+})
+
+console.log( displayData )
