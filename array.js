b1d6cdd621343328ba2f386bcac43791aae3e46e
FrancisGichuhi <francisgichh@gmail.com>
2019-11-13 21:01:31 -0500

    .env-examples

---
+++ array.js
@@ -0,0 +1,18 @@
+var favoriteNumber = ["10","11","20"];
+
+var i = 1;
+while ( i<= favoriteNumber.length) {
+    if (i == 1) {
+    console.log('My first choice is' + ' ' + favoriteNumber[0])
+
+}
+if (i == 2) {
+    console.log('My second choice is' + ' ' + favoriteNumber[1])
+
+}
+if (i == 3) {
+    console.log('My third choice is' + ' ' + favoriteNumber[2])
+
+}
+    i++
+} 
---
+++ loop.js
@@ -0,0 +1,23 @@
+*/function grader (numGrade) {
+    if (numGrade >= 80 && numGrade <= 88) {
+        console.log("B");
+    }
+    else if (numGrade >= 88 && numGrade <= 90) {
+        console.log("B");
+    }   
+    else if (numGrade >= 90 && numGrade <= 91) {
+        console.log("B");
+    }
+    else if (numGrade >= 91 && numGrade <= 95) {
+        console.log("B");   
+    }
+
+    else {
+        console.log("Logging each grade point in the range")
+    }
+    
+}
+
+let grade = 87;
+grader(grade); 
+/*
